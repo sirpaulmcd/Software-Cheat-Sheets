e8108b76e6d17b8d1a04f5d4bf593fa522be7e6d
Paul McDonald <sirpaulmcd@tutanota.com>
2020-11-02 15:47:27 -0700

    Added git basics tutorial

---
+++ Git/Git-Basics/Converter.py
@@ -0,0 +1,25 @@
+class Converter():
+    '''
+    A class used to convert units of measurement.
+
+    ### Authors
+    Person A, Person B, Person C
+    '''
+
+    def centimetersToInches(self, centimeter_value):
+        # TODO: Person A will implement this method
+        return 0
+    
+    def celsiusToFahrenheit(self, celsius_value):
+        # TODO: Person B will implement this method
+        return 0
+
+    def kilogramsToPounds(self, kilogram_value):
+        # TODO: Person C will implement this method
+        return 0
+
+if __name__ == "__main__":
+    my_converter = Converter()
+    print("10 centimeters is", my_converter.centimetersToInches(10), "inches.")
+    print("10 celsius is", my_converter.celsiusToFahrenheit(10), "fahrenheit.")
+    print("10 kilograms is", my_converter.kilogramsToPounds(10), "pounds.")
